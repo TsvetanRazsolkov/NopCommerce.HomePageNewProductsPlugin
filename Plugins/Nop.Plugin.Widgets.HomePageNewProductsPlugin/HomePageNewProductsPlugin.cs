7617cc1c0e5d3a7760324c674234ce68b8c6cdeb
TsvetanRazsolkov <tsvetan.razsolkov@gmail.com>
2017-02-16 17:08:22 +0200

    Added nop commerce and bare plugin project.

---
+++ Plugins/Nop.Plugin.Widgets.HomePageNewProductsPlugin/HomePageNewProductsPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nop.Core.Plugins;
+using Nop.Services.Cms;
+
+namespace Nop.Plugin.Widgets.HomePageNewProductsPlugin
+{
+    public class HomePageNewProductsPlugin : BasePlugin, IWidgetPlugin
+    {
+        public void GetConfigurationRoute(out string actionName, out string controllerName, out System.Web.Routing.RouteValueDictionary routeValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetDisplayWidgetRoute(string widgetZone, out string actionName, out string controllerName, out System.Web.Routing.RouteValueDictionary routeValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<string> GetWidgetZones()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
